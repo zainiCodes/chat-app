95d7fe6b56ad5f15968ff0e84046e71574451b04
zainiCodes <zainulabdinpk19@gmail.com>
2026-05-19 14:16:09 +0500

    Revert "feat: add friends feature with profile, list, and reusable form field components"
    This reverts commit 6ea5f36facfa7b40a57aec638de08b86e95ab259.

--- apps/web/src/features/friends/components/friend-profile.tsx
+++
@@ -1,5 +0,0 @@
-export default function FriendsProfile() {
-    return (
-        <div>FriendsProfile</div>
-    )
-}
--- apps/web/src/features/friends/index.tsx
+++ apps/web/src/features/friends/index.tsx
@@ -1,22 +1,5 @@
-import { Input } from "@chat-app/ui/components/input";
-import { Field } from "@chat-app/ui/components/field";
-import FriendsProfile from "./components/friend-profile";
-import FriendsList from "./components/friends-list";
-import { Button } from "@chat-app/ui/components/button";
-
 export default function FriendsComponent() {
     return (
-        <div className="flex gap-4">
-            <div className="w-2/5 ">
-                <Field orientation="horizontal">
-                    <Input type="search" placeholder="Search..." />
-                    <Button>Search</Button>
-                </Field>
-                <FriendsList />
-            </div>
-            <div className="w-3/5 ">
-                <FriendsProfile />
-            </div>
-        </div>
+        <div>index</div>
     )
 }
